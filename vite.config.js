bd63920b14d8cacd5817e16e3138852f78770aa7
Rudy <zzrudy1998zz@gmail.com>
2026-05-21 12:36:48 -0500

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/visualizador-360-web/',
   plugins: [react()],
   resolve: {
     dedupe: ['three']
